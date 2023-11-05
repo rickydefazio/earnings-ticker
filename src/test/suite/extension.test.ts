73beede6890f1a6fc22c9d6da7057c2202563aef
Ricky DeFazio <rickysdefazio@gmail.com>
2023-11-05 16:28:20 -0500

    remove release notes

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,7 +3,13 @@ import * as vscode from 'vscode';
 import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
-  vscode.window.showInformationMessage('Start all tests.');
+  before(() => {
+    vscode.window.showInformationMessage('Starting all tests!');
+  });
+
+  after(() => {
+    vscode.window.showInformationMessage('All tests done!');
+  });
 
   test('Sample test', () => {});
 });
